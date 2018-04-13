6e98da2a4faaeed3338be43e04dae7ab06f8ee72
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-04-13 00:45:40 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+// bundle.js (post Webpack digestion)
+function reveal(person, realIdentity) {
+  person.identity = realIdentity
+}
+ 
+const gutMensch = {
+  name: "Andrew Cohn",
+  identity: "Friendly Neighborhood Flatiron Teacher",
+}
+ 
+reveal(gutMensch, "Chrome Boi")
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
